4831fb9d603a3dd808ea2a1a9c85efedeae6c6c1
Hi-yano <begin_2020@icloud.com>
2021-08-31 12:21:37 +0900

    Bitcoin価格の取得

--- 05-BitCoin/main.js
+++ 05-BitCoin/main.js
@@ -4,7 +4,11 @@ var app = new Vue({
     bpi: null
   },
   mounted: function() {
-    
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(function(response) {
+      // console.log(response.data.bpi)
+      // console.log(response.data.bpi.USD.rate_float)
+      this.bpi = response.data.bpi
+    }.bind(this))
   }
-
 })
