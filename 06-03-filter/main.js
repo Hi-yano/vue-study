5f076d2bccc8f259cd37b06e999b267c2aa30a78
Hi-yano <begin_2020@icloud.com>
2021-09-04 16:39:23 +0900

    フィルタの連結

--- 06-03-filter/main.js
+++ 06-03-filter/main.js
@@ -1,8 +1,25 @@
+// グローバルフィルタはVueインスタンスが定義される前に記述する必要がある
+// Vue.filter('numberFormat', function(value) {
+//   return value.toLocaleString()
+// })
+
+// 以下フィルタの連結のためのグローバルフィルタ
+// USDに変換するためのフィルタ
+Vue.filter('toUSD', function(jpy){
+  return jpy/100
+})
+// 数値を三桁づつコンマで区切るためのフィルタ
+Vue.filter('numberFormat', function(value){
+  return value.toLocaleString()
+})
+
 var app = new Vue({
   el: '#app',
   data: {
-    price: 29800
+    price: 29800,
+    jpyPrice: 298000
   },
+  // 以下はローカルフィルタ
   filters: {
     numberFormat: function(value) {
       return value.toLocaleString()
