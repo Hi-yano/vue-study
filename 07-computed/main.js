98b434a7bf40921702eb95730bb3aa5e3452c2b1
Hi-yano <begin_2020@icloud.com>
2021-09-05 09:03:16 +0900

    算出プロパティのgetterとsetter

--- 07-computed/main.js
+++ 07-computed/main.js
@@ -1,11 +1,20 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue.js!'
+    message: 'Hello Vue.js!',
+    basePrice: 100
   },
   computed: {
     reversedMessage: function() {
       return this.message.split('').reverse().join('')
+    },
+    taxIncludedPrice: {
+      get: function() {
+        return parseInt(this.basePrice * 1.08) // parseIntは整数を返す関数
+      },
+      set: function(taxIncludedPrice) {
+        this.basePrice = Math.ceil(taxIncludedPrice / 1.08) // Math.ceil 小数点以下の値を切り上げした値を返す関数
+      }
     }
   },
   // メソッドで実装した場合
