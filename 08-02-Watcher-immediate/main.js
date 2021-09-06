19a4072ca27b3f7661ba0bcdb2d0f791f946d76f
Hi-yano <begin_2020@icloud.com>
2021-09-06 22:30:57 +0900

    immediate

--- 08-02-Watcher-immediate/main.js
+++ 08-02-Watcher-immediate/main.js
@@ -1,4 +1,22 @@
 var app = new Vue({
   el: '#app',
-  
-  })
+  data: {
+    colors: [
+      { name: 'Red' },
+      { name: 'Green' },
+      { name: 'Blue' }
+    ]
+  },
+  watch: {
+    colors: {
+      handler: function(newValue, oldValue) {
+        console.log('Update!')
+        console.log('new: %s, oldValue: %s',
+        JSON.stringify(newValue, null, '\t'),
+        JSON.stringify(oldValue, null, '\t') )
+      },
+      deep: true,
+      immediate: true //immediate = 「即座に」
+    }
+  }
+})
