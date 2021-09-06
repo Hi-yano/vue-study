eb89a82395a1070029fbfe7050fe3062cc9bf5fb
Hi-yano <begin_2020@icloud.com>
2021-09-06 21:52:23 +0900

    算出プロパティと監視プロパティの比較

--- 08-Watcher/main.js
+++ 08-Watcher/main.js
@@ -4,7 +4,15 @@ var app = new Vue({
     message: 'Hello Vue.js!',
     km: 0,
     m: 0,
-    mm: 0
+    mm: 0,
+    firstName: '',
+    lastName: '',
+    // fullName: '',  // 算出プロパティでの実装の場合削除する
+  },
+  computed: { //算出プロパティ(computed)
+    fullName: function() {
+      return this.firstName + ' ' + this.lastName
+    }
   },
   watch: {
     message: function(newValue, oldValue) {
@@ -27,6 +35,12 @@ var app = new Vue({
       this.km = value / 1000000
       this.m = value / 1000
       this.mm = value
-    }
+    },
+    // firstName: function(value) {  // 算出プロパティでの実装の場合削除する
+    //   this.fullName = value + ' ' + this.lastName
+    // },
+    // lastName: function(value) { // 算出プロパティでの実装の場合削除する
+    //   this.fullName = this.firstName + ' ' + this.lastName
+    // }
   }
   })
