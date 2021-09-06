273f3ba97a22bb6cbc9f0007c7a3d4a53c7a2c3a
Hi-yano <begin_2020@icloud.com>
2021-09-06 22:12:35 +0900

    deep-1

--- 08-01-Watcher-Deep/main.js
+++ 08-01-Watcher-Deep/main.js
@@ -1,4 +1,19 @@
 var app = new Vue({
   el: '#app',
-  
+  data: {
+    colors: [
+      { name: 'Red' },
+      { name: 'Green' },
+      { name: 'Blue' }
+    ]
+  },
+  watch: {
+    colors: {
+      handler: function(newValue, oldValue) {
+        console.log('Update!')
+      },
+      deep: true    // false にして値を変更すると色は変更されるが、ネストされた値の変更は監視されない。
+    // ちなみに deep オプションを省略した場合は false と同じ動作になる
+    }
+  }
   })
