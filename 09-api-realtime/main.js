f0ea11d65cbd63c567cb274acc0d93b16ff55698
Hi-yano <begin_2020@icloud.com>
2021-09-07 22:23:56 +0900

    dataオプションにプロパティを用意

--- 09-api-realtime/main.js
+++ 09-api-realtime/main.js
@@ -1,7 +1,14 @@
 var app = new Vue({
   el: '#app',
+  /*data オプション
+  ・ items  ・・・APIから取得した検索結果を格納
+  ・ keyword・・・ユーザーが入力した検索キー
+  ・ message・・・ユーザーに表示するメッセージ
+  */
   data: {
-
+    items: null,
+    keyword: '',
+    message: ''
   },
   watch: {
 
@@ -10,6 +17,6 @@ var app = new Vue({
 
   },
   methods: {
-    
+
   }
   })
