fca9cb202d93ddf5c7b36f3ba7c5667fc80d8486
Hi-yano <begin_2020@icloud.com>
2021-09-07 23:06:52 +0900

    完成

--- 09-api-realtime/main.js
+++ 09-api-realtime/main.js
@@ -10,17 +10,23 @@ var app = new Vue({
     keyword: '',
     message: ''
   },
-  watch: {
-
+  watch: {  //フォームの監視
+    keyword: function(newKeyword, oldKeyword) {
+      // console.log(newKeyword)
+      this.message = 'Waiting for you to stop typing...'
+      this.debouncedGetAnswer()
+    }
   },
   created: function() {
-    this.keyword = 'JavaScript'
-    this.getAnswer()
+    // this.keyword = 'JavaScript'
+    // this.getAnswer()
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000)  // ミリ秒で指定。指定時間内に同じイベントが発生すると処理は実行せず、発生しなければ処理を実行する → APIへのリクエストを間引くことができる。
   },
   methods: {
     getAnswer: function() {
       if(this.keyword === '') {
-        this.item = null
+        this.items = null
+        this.message = ''
         return
       }
       this.message = 'Loading...'
